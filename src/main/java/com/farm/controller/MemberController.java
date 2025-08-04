9ec2e502db9df7596ca195681940f48245687c8e
GSunwoo <ace9sun@naver.com>
2025-08-04 21:42:23 +0900

    MemberDTO에 사업장 관련 변수 추가

--- src/main/java/com/farm/controller/MemberController.java
+++ src/main/java/com/farm/controller/MemberController.java
@@ -2,9 +2,6 @@ package com.farm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import com.farm.dto.MemberDTO;
 
 @Controller
 public class MemberController {
--- src/main/java/com/farm/dto/MemberDTO.java
+++ src/main/java/com/farm/dto/MemberDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class MemberDTO {
+	// 구매자, 판매자 공통
 	private String user_type;
 	private String user_id;
 	private String user_pw;
@@ -13,5 +14,14 @@ public class MemberDTO {
 	private String zipcode;
 	private String addr1;
 	private String addr2;
+	
+	// 판매자용(사업장 관련 정보)
 	private String farm_id;
+	private String owner_name;
+	private String brand_name;
+	private String com_zip;
+	private String com_addr1;
+	private String com_addr2;
+	private String entryman;
+	private String account;
 }
