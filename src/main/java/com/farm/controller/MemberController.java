a40e57263cc61e4d34a50bfcd7e74c60514d0bfc
GSunwoo <ace9sun@naver.com>
2025-08-06 20:59:50 +0900

    password 인코딩해서 저장

--- src/main/java/com/farm/controller/MemberController.java
+++ src/main/java/com/farm/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.farm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,11 +28,17 @@ public class MemberController {
 	
 	@PostMapping("/memberForm/seller.regist.do") // 서브밋 했을때 가입시켜주는
 	public String memberRegistSeller(MemberDTO memberDTO) {
+		String passwd = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+					.encode(memberDTO.getUser_pw());
+		memberDTO.setUser_pw(passwd);
 		formDao.registMember(memberDTO);
 		return "redirect:/";
 	}
 	@PostMapping("/memberForm/buyer.regist.do")
 	public String memberRegistBuyer(MemberDTO memberDTO) {
+		String passwd = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+				.encode(memberDTO.getUser_pw());
+		memberDTO.setUser_pw(passwd);
 		formDao.registMember(memberDTO);
 		return "redirect:/";
 	}
