5bbd26e2ecd4fe683197cbe7e7176855c2f554ff
dudns5552 <dudns5552@gmail.com>
2025-08-06 11:05:56 +0900

    나영운 커밋

--- src/main/java/com/farm/entity/Address.java
+++ src/main/java/com/farm/entity/Address.java
@@ -1,5 +1,6 @@
 package com.farm.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -25,8 +26,11 @@ public class Address {
 			allocationSize = 1
 		)
 	@GeneratedValue(generator = "addressSequence")
+	@Column(nullable = false)
 	private Long addr_id;
+	@Column(nullable = false)
 	private String zipcode;
+	@Column(nullable = false)
 	private String addr1;
 	private String addr2;
 }
