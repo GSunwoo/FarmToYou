93b68953e7ffc8a1f051a0fbbe758976bd702273
GSunwoo <ace9sun@naver.com>
2025-08-07 18:19:32 +0900

    mypage 판매자 페이지 데이터 전달 추가 및 null 처리

--- src/main/java/com/farm/controller/MypageController.java
+++ src/main/java/com/farm/controller/MypageController.java
@@ -29,19 +29,37 @@ public class MypageController {
 	
 	@GetMapping("/buyer/mypage.do")
 	public String buyerMypage(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
-		MemberDTO member = userDetails.getMemberDTO();
-		Long member_id = member.getMember_id();
-		int orderCnt = dao.getOrderCnt(member_id);
-		int reviewCnt = dao.getReviewCnt(member_id);
-		int inquiryCnt = dao.getInquiryCnt(member_id);
-		model.addAttribute("member", member);
-		model.addAttribute("orderCnt", orderCnt);
-		model.addAttribute("reviewCnt", reviewCnt);
-		model.addAttribute("inquiryCnt", inquiryCnt);
+		if(userDetails!=null) {
+			MemberDTO member = userDetails.getMemberDTO();
+			Long member_id = member.getMember_id();
+			int orderCnt = dao.getOrderCnt(member_id);
+			int reviewCnt = dao.getReviewCnt(member_id);
+			int inquiryCnt = dao.getInquiryCnt(member_id);
+			model.addAttribute("member", member);
+			model.addAttribute("orderCnt", orderCnt);
+			model.addAttribute("reviewCnt", reviewCnt);
+			model.addAttribute("inquiryCnt_buyer", inquiryCnt);
+		}
 		return "buyer/mypage";
 	}
 	@GetMapping("/seller/mypage.do")
-	public String sellerMypage() {
+	public String sellerMypage(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+		if(userDetails!=null) {
+			MemberDTO member = userDetails.getMemberDTO();
+			Long member_id = member.getMember_id();
+			int chkOrder = dao.getCheckOrder(member_id);
+			int preOrder = dao.getPrepareOrder(member_id);
+			int deliOrder = dao.getDeliOrder(member_id);
+			int cmplOrder = dao.getCompleteOrder(member_id);
+			int inquiryCnt = dao.getInquiryCnt_Seller(member_id);
+			
+			model.addAttribute("member", member);
+			model.addAttribute("chkOrder", chkOrder);
+			model.addAttribute("preOrder", preOrder);
+			model.addAttribute("deliOrder", deliOrder);
+			model.addAttribute("cmplOrder", cmplOrder);
+			model.addAttribute("inquiryCnt_seller", inquiryCnt);
+		}
 		return "seller/mypage";
 	}
 }
--- src/main/java/com/farm/service/IMypageService.java
+++ src/main/java/com/farm/service/IMypageService.java
@@ -4,7 +4,16 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface IMypageService {
-	public int getOrderCnt(Long member_id);
-	public int getReviewCnt(Long member_id);
-	public int getInquiryCnt(Long member_id);
+	// 구매자용
+	public int getOrderCnt(Long member_id); // 주문수
+	public int getReviewCnt(Long member_id); // 리뷰수
+	public int getInquiryCnt(Long member_id); // 문의수
+	
+	// 판매자용
+	public int getCheckOrder(Long member_id); // 주문확인중
+	public int getPrepareOrder(Long member_id); // 상품준비중
+	public int getDeliOrder(Long member_id); // 배송중
+	public int getCompleteOrder(Long member_id); // 배송완료
+	public int getInquiryCnt_Seller(Long member_id); // 문의수(판매자)
+	
 }
