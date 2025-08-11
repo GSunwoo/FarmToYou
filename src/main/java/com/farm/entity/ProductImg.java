232f9342f6e6848406cb0f3ab377d8dfa2d8a1ee
GSunwoo <ace9sun@naver.com>
2025-08-11 20:38:41 +0900

    상품/리뷰/상호 이미지 entity 생성 & review테이블 visitcount 삭제

---
+++ src/main/java/com/farm/entity/ProductImg.java
@@ -0,0 +1,38 @@
+package com.farm.entity;
+
+import java.sql.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity
+public class ProductImg {
+	@Id
+	@SequenceGenerator(
+			name = "productImgSequence",
+			sequenceName = "seq_productimg_id",
+			initialValue = 1,
+			allocationSize = 1
+		)
+	@GeneratedValue(generator = "productImgSequence")
+	private Long prodimg_id;
+	private String filename;
+	
+	@ManyToOne
+	@JoinColumn(name = "prod_id", nullable = false)
+    private Product product; // 상품
+}
---
+++ src/main/java/com/farm/entity/ReviewImg.java
@@ -0,0 +1,36 @@
+package com.farm.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity
+public class ReviewImg {
+	@Id
+	@SequenceGenerator(
+			name = "reviewImgSequence",
+			sequenceName = "seq_reviewimg_id",
+			initialValue = 1,
+			allocationSize = 1
+		)
+	@GeneratedValue(generator = "reviewImgSequence")
+	private Long reviewimg_id;
+	private String filename; // 서버에 저장될 파일의 이름
+	
+	@ManyToOne
+	@JoinColumn(name = "review_id", nullable = false)
+    private Review review; // 리뷰
+}
+
