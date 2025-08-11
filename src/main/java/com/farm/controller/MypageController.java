ee2eed2f7efe4091d17fa2ece0dc76500cc24523
GSunwoo <ace9sun@naver.com>
2025-08-11 16:31:25 +0900

    마이페이지 일간 판매량/매출 데이터

--- src/main/java/com/farm/controller/MypageController.java
+++ src/main/java/com/farm/controller/MypageController.java
@@ -1,5 +1,12 @@
 package com.farm.controller;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -21,6 +28,9 @@ public class MypageController {
 		MemberDTO member = userDetails.getMemberDTO();
 		if(member.getUser_type().equals("ROLE_BUYER")) {
 			return "redirect:/buyer/mypage.do";
+		}
+		else if(member.getUser_type().equals("ROLE_ADMIN")) {
+			return "redirect:/admin/mypage.do";
 		}
 		else {
 			return "redirect:/seller/mypage.do";
@@ -45,20 +55,51 @@ public class MypageController {
 	@GetMapping("/seller/mypage.do")
 	public String sellerMypage(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
 		if(userDetails!=null) {
+			// 현재 사용자 불러오기
 			MemberDTO member = userDetails.getMemberDTO();
 			Long member_id = member.getMember_id();
-			int chkOrder = dao.getCheckOrder(member_id);
-			int preOrder = dao.getPrepareOrder(member_id);
-			int deliOrder = dao.getDeliOrder(member_id);
-			int cmplOrder = dao.getCompleteOrder(member_id);
-			int inquiryCnt = dao.getInquiryCnt_Seller(member_id);
 			
+			/******  상품 주문 현황  ******/
+			int chkOrder = dao.getCheckOrder(member_id);    // 주문확인중
+			int preOrder = dao.getPrepareOrder(member_id);  // 상품준비중
+			int deliOrder = dao.getDeliOrder(member_id);    // 배송중
+			int cmplOrder = dao.getCompleteOrder(member_id);// 배송완료
 			model.addAttribute("member", member);
 			model.addAttribute("chkOrder", chkOrder);
 			model.addAttribute("preOrder", preOrder);
 			model.addAttribute("deliOrder", deliOrder);
 			model.addAttribute("cmplOrder", cmplOrder);
+			
+			/****** 상품문의현황  ******/
+			int inquiryCnt = dao.getInquiryCnt_Seller(member_id); // 현재 문의 수
 			model.addAttribute("inquiryCnt_seller", inquiryCnt);
+			
+			/****** 상품판매실적  ******/
+			LocalDate today = LocalDate.now();			// 오늘날짜
+			LocalDate fourDaysAgo = today.minusDays(4); // 4일전
+			
+			// sql Date로 변환
+			Date sqlToday = Date.valueOf(today);
+			Date sqlFourDaysAgo = Date.valueOf(fourDaysAgo);
+			
+			List<Date> dateList = new ArrayList<>();
+			for(int i = 0; i<4; i++) {
+				dateList.add(Date.valueOf(today.minusDays(i)));
+			}
+			
+			List<Integer> soldNum = dao.getSoldNum(member_id, sqlToday, sqlFourDaysAgo); // 지난 5일 판매량
+			List<Integer> sales = dao.getSales(member_id, sqlToday, sqlFourDaysAgo);     // 지난 5일 매출
+			
+			Map<String, Object> soldData = new HashMap<>();
+			
+			for(int i = 0; i<5; i++) {
+				soldData.put("date",dateList);
+				soldData.put("sold",soldNum);
+				soldData.put("sales",sales);
+			}
+			// 날짜별 판매량/매출(지난 5일간)
+			// Object로 보내서 list로 변환필요
+			model.addAttribute(soldData);
 		}
 		return "seller/mypage";
 	}
--- src/main/java/com/farm/service/IMypageService.java
+++ src/main/java/com/farm/service/IMypageService.java
@@ -1,19 +1,30 @@
 package com.farm.service;
 
+import java.sql.Date;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface IMypageService {
 	// 구매자용
-	public int getOrderCnt(Long member_id); // 주문수
-	public int getReviewCnt(Long member_id); // 리뷰수
-	public int getInquiryCnt(Long member_id); // 문의수
+	public int getOrderCnt(@Param("member_id") Long member_id); // 주문수
+	public int getReviewCnt(@Param("member_id")Long member_id); // 리뷰수
+	public int getInquiryCnt(@Param("member_id") Long member_id); // 문의수
 	
 	// 판매자용
-	public int getCheckOrder(Long member_id); // 주문확인중
-	public int getPrepareOrder(Long member_id); // 상품준비중
-	public int getDeliOrder(Long member_id); // 배송중
-	public int getCompleteOrder(Long member_id); // 배송완료
-	public int getInquiryCnt_Seller(Long member_id); // 문의수(판매자)
+	public int getCheckOrder(@Param("member_id") Long member_id); // 주문확인중
+	public int getPrepareOrder(@Param("member_id") Long member_id); // 상품준비중
+	public int getDeliOrder(@Param("member_id") Long member_id); // 배송중
+	public int getCompleteOrder(@Param("member_id") Long member_id); // 배송완료
+	public int getInquiryCnt_Seller(@Param("member_id") Long member_id); // 문의수(판매자)
 	
+	public List<Integer> getSoldNum(@Param("member_id") Long member_id,
+						@Param("start_date") Date start_date,
+						@Param("end_date")	Date end_date);
+	
+	public List<Integer> getSales(@Param("member_id") Long member_id,
+			@Param("start_date") Date start_date,
+			@Param("end_date")	Date end_date);
 }
