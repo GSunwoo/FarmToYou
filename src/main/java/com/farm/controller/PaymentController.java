78594f639cea7e669b6623c31520a2bf823d94c7
GSunwoo <ace9sun@naver.com>
2025-08-14 20:46:27 +0900

    결제 api 연결

--- src/main/java/com/farm/controller/PaymentController.java
+++ src/main/java/com/farm/controller/PaymentController.java
@@ -1,5 +1,7 @@
 package com.farm.controller;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -15,25 +17,41 @@ public class PaymentController {
 	@Value("${toss.clientKey}")
 	private String WIDGET_CLIENT_KEY; // toss 클라이언트 키
 	
-	@GetMapping("/buyer/pay/checkout")
+	@GetMapping("/buyer/pay/checkout.do")
 	public String checkout(Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
 		model.addAttribute("widgetClientKey",WIDGET_CLIENT_KEY);
 		// customerKey를 만들기위해 member_id와 user_id를 Bcrypt로 인코딩 후 model객체로 전달
 		Long memberId = userDetails.getMemberDTO().getMember_id();
 		String userId = userDetails.getUsername();
-		String encoId = PasswordEncoderFactories.createDelegatingPasswordEncoder()
-				.encode(memberId.toString()+userId);
-		model.addAttribute("customerKey",encoId.replace("{bcrypt}", ""));
-		return "checkout";
+		// customerKey 제작
+		String encoId = generateRandomString(memberId.toString()+userId,20);
+		model.addAttribute("customerKey", encoId);
+		return "pay/checkout";
 	}
 	
-	@GetMapping("/buyer/pay/success")
+	@GetMapping("/buyer/pay/success.do")
 	public String success() {
-		return "success";
+		return "pay/success";
 	}
 	
-	@GetMapping("/buyer/pay/fail")
+	@GetMapping("/buyer/pay/fail.do")
 	public String fail() {
-		return "fail";
+		return "pay/fail";
 	}
+	
+	public static String generateRandomString(String input, int length) {
+        // 입력값의 해시코드를 시드로 사용
+        long seed = input.hashCode();
+        Random random = new Random(seed);
+
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int idx = random.nextInt(chars.length());
+            sb.append(chars.charAt(idx));
+        }
+
+        return sb.toString();
+    }
 }
--- src/main/resources/static/js/checkout.js
+++ src/main/resources/static/js/checkout.js
@@ -8,8 +8,6 @@ console.log("checkout")
 // TODO: clientKey는 개발자센터의 결제위젯 연동 키 > 클라이언트 키로 바꾸세요.
 // TODO: 구매자의 고유 아이디를 불러와서 customerKey로 설정하세요. 이메일・전화번호와 같이 유추가 가능한 값은 안전하지 않습니다.
 // @docs https://docs.tosspayments.com/reference/widget-sdk#sdk-설치-및-초기화
-const widgetClientKey = `${widgetClientKey}`;
-const customerKey = `${customerKey}`;
 const paymentWidget = PaymentWidget(widgetClientKey, customerKey); // 회원 결제
 // const paymentWidget = PaymentWidget(widgetClientKey, PaymentWidget.ANONYMOUS); // 비회원 결제
 
@@ -45,8 +43,8 @@ button.addEventListener("click", function() {
 	paymentWidget.requestPayment({
 		orderId: generateRandomString(),
 		orderName: "토스 티셔츠 외 2건",
-		successUrl: window.location.origin + "/success",
-		failUrl: window.location.origin + "/fail",
+		successUrl: window.location.origin + "/buyer/pay/success.do",
+		failUrl: window.location.origin + "/buyer/pay/fail.do",
 		customerEmail: "customer123@gmail.com",
 		customerName: "김토스",
 		customerMobilePhone: "01012341234",
