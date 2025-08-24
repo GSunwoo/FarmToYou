d58453d061ffddab1a81cfe0dd6283eff673e41b
dudns5552 <dudns5552@gmail.com>
2025-08-24 18:47:11 +0900

    상품문의 댓글 추가중

--- src/main/java/com/farm/entity/Inquiry.java
+++ src/main/java/com/farm/entity/Inquiry.java
@@ -3,13 +3,17 @@ package com.farm.entity;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.SequenceGenerator;
 import lombok.AccessLevel;
@@ -47,7 +51,10 @@ public class Inquiry {
 	private Member member;
 	@ManyToOne
 	@JoinColumn(name = "prod_id", nullable = false)
-	private Product product;
+	private Product product; 
+	
+	@OneToMany(mappedBy = "inquiry", cascade = CascadeType.REMOVE)
+    private List<Comment> comment = new ArrayList<>();
 	
 	@PrePersist
 	protected void onPrePersist() {
